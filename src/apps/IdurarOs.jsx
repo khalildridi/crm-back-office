246d70cd0ef2a821a1a6a49f5547e4a06cc666ed
khalil dridi <khalildridii21@gmail.com>
2024-06-18 14:37:34 +0100

    fixes cross origin

--- src/apps/IdurarOs.jsx
+++ src/apps/IdurarOs.jsx
@@ -22,10 +22,11 @@ const DefaultApp = () => (
 
 export default function IdurarOs() {
   const { isLoggedIn } = useSelector(selectAuth);
+  console.log("is logged in",isLoggedIn)
 
-  console.log(
-    '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
-  );
+  // console.log(
+  //   '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+  // );
 
   // // Online state
   // const [isOnline, setIsOnline] = useState(navigator.onLine);
