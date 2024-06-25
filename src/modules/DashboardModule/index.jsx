5e92238a7dc36de71ba9632315b89ba48930a455
Ben Maghzouz Raed <raed.maghzouz@gmail.com>
2024-06-26 00:35:36 +0100

    style dashboard

--- src/modules/DashboardModule/index.jsx
+++ src/modules/DashboardModule/index.jsx
@@ -155,13 +155,6 @@ export default function DashboardModule() {
             isLoading={quoteLoading}
             data={quoteResult?.total}
           />
-          {/* <SummaryCard
-            title={translate('offers')}
-            tagColor={'green'}
-            prefix={translate('This month')}
-            isLoading={offerLoading}
-            data={offerResult?.total}
-          /> */}
           <SummaryCard
             title={translate('Unpaid')}
             tagColor={'red'}
@@ -169,17 +162,24 @@ export default function DashboardModule() {
             isLoading={invoiceLoading}
             data={invoiceResult?.total_undue}
           />
+          <SummaryCard
+            title={'Largest investment'}
+            tagColor={'green'}
+            prefix={translate('This month')}
+            isLoading={offerLoading}
+            data={0}
+          />
         </Row>
         <div className="space30"></div>
         <Row gutter={[32, 32]}>
-          <Col className="gutter-row w-full" sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 18 }}>
+          <Col className="gutter-row w-full" sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 12 }}>
             <div className="whiteBox shadow" style={{ height: 458 }}>
               <Row className="pad20" gutter={[0, 0]}>
                 {statisticCards}
               </Row>
             </div>
           </Col>
-          <Col className="gutter-row w-full" sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 6 }}>
+          <Col className="gutter-row w-full" sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 12 }}>
             <CustomerPreviewCard
               isLoading={clientLoading}
               activeCustomer={clientResult?.active}
@@ -189,7 +189,7 @@ export default function DashboardModule() {
         </Row>
         <div className="space30"></div>
         <Row gutter={[32, 32]}>
-          <Col className="gutter-row w-full" sm={{ span: 24 }} lg={{ span: 12 }}>
+          <Col className="gutter-row w-full" sm={{ span: 24 }} lg={{ span: 24 }}>
             <div className="whiteBox shadow pad20" style={{ height: '100%' }}>
               <h3 style={{ color: '#22075e', marginBottom: 5, padding: '0 20px 20px' }}>
                 {translate('Recent Invoices')}
@@ -199,14 +199,14 @@ export default function DashboardModule() {
             </div>
           </Col>
 
-          <Col className="gutter-row w-full" sm={{ span: 24 }} lg={{ span: 12 }}>
+          {/* <Col className="gutter-row w-full" sm={{ span: 24 }} lg={{ span: 12 }}>
             <div className="whiteBox shadow pad20" style={{ height: '100%' }}>
               <h3 style={{ color: '#22075e', marginBottom: 5, padding: '0 20px 20px' }}>
                 {translate('Recent Quotes')}
               </h3>
               <RecentTable entity={'quote'} dataTableColumns={dataTableColumns} />
             </div>
-          </Col>
+          </Col> */}
         </Row>
       </>
     );
